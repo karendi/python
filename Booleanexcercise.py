18ea54582ea0ec42634ccea3b84232ccc1f537a9
karendi <sharonkarendi16@gmail.com>
2016-07-15 15:42:02 +0300

    boolean excercise

---
+++ Booleanexcercise.py
@@ -0,0 +1,23 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people<cats:
+    print("too many cats")
+
+if people > cats:
+    print("less cats")
+
+if people <dogs:
+    print("too many dogs")
+
+dogs += 5
+
+if people >= dogs:
+    print("people are greater than or equal to dogs")
+
+if people <= dogs:
+    print("People are less than or equal to dogs")
+
+if people == dogs:
+    print("People are equal to dogs")
